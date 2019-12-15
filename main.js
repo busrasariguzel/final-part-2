1a3b5b48f9f38861c44f257c82180d122c7aae6a
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-12-15 14:49:39 -0500

    new buttons added

--- main.js
+++ main.js
@@ -1,42 +1,25 @@
 document.querySelector("#remove-all-button").addEventListener('click', function(){
-    list = document.querySelector('#symptoms')
-    
-    list.style.visibility = "hidden";
+const theWholeList = document.querySelectorAll("#symptoms li")
+for (const item of theWholeList) {
+item.style.display='none'
 }
-)
-
-
-
+})
 
 document.querySelector("#add-button").addEventListener('click', function(){
 list = document.querySelector('#symptoms')
-// list.style.visibility = "initial";
 newLi = document.createElement('li')
 symptomsList = document.querySelector('#input').value
 document.querySelector('#symptoms').appendChild(newLi).innerText = symptomsList 
 })
 
-// document.querySelector("#remove-button").addEventListener('click', function(event){
-//     document.querySelector('li')
-//     if (event.target.style.textDecoration === 'line-through'){
-//         target.style.visibility = "hidden";
-//     }
-//         }
-// )
-
 document.querySelector('#symptoms').addEventListener('click', function(event) {
-// event.target.style.textDecoration = 'line-through'
 event.target.classList.toggle('yes')
 })
-
-
 
 document.querySelector('#remove-button').addEventListener('click', function(event) {
 const theWholeList = document.querySelectorAll(".yes")
 for (const item of theWholeList) {
-item.addEventListener('click', function(event) {
-    event.target.classList.add('remove')
-})
+item.classList.add('remove')
 }
 })
 //  SECOND LIST FUNCTIONS #####
@@ -50,4 +33,17 @@ list = document.querySelector('#symptoms-2')
 newLi = document.createElement('li')
 symptomsList2 = document.querySelector('#input-2').value
 document.querySelector('#symptoms-2').appendChild(newLi).innerText = symptomsList2
-        })
+})
+
+document.querySelector('#remove-button-2').addEventListener('click', function(event) {
+const theWholeList = document.querySelectorAll(".yes")
+for (const item of theWholeList) {
+item.classList.add('remove')
+}
+})      
+document.querySelector("#remove-all-button-2").addEventListener('click', function(){
+const theWholeList = document.querySelectorAll("#symptoms-2 li")
+for (const item of theWholeList) {
+item.style.display='none'
+}
+})
