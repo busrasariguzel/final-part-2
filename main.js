ae920643f6ac059ff6d6aa7a3551061383c22e93
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-12-16 10:18:43 -0500

    added github link and about link

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.querySelector('#symptoms').appendChild(newLi).innerText = symptomsList
 })
 
 document.querySelector('#symptoms').addEventListener('click', function(event) {
+event.target.style.textDecoration = 'line-through'
 event.target.classList.toggle('yes')
 })
 
